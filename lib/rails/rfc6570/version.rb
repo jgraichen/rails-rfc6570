61880e37536de8da511c070c21c6615996f12166
Jan Graichen <jg@altimos.de>
2014-10-22 19:31:36 +0200

    Release 0.2.0
    * Added *_path_rfc6570 and *_url_rfc6570 (@arantir)

--- lib/rails/rfc6570/version.rb
+++ lib/rails/rfc6570/version.rb
@@ -2,8 +2,8 @@ module Rails
   module RFC6570
     module VERSION
       MAJOR = 0
-      MINOR = 1
-      PATCH = 3
+      MINOR = 2
+      PATCH = 0
       STAGE = nil
 
       STRING = [MAJOR, MINOR, PATCH, STAGE].reject(&:nil?).join('.').freeze
