8289a6e1b7ab4753b35750d5d6e0d8fff600c83b
Jan Graichen <jgraichen@altimos.de>
2018-08-21 21:41:25 +0200

    Actually add rubocop to Gemfile and run it

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,16 +6,15 @@ Coveralls.wear! do
   add_filter 'spec'
 end
 
-#
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../dummy/config/environment', __FILE__)
+require File.expand_path('dummy/config/environment', __dir__)
 require 'rspec/rails'
 
 Rails.backtrace_cleaner.remove_silencers!
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[File.expand_path('../support/**/*.rb', __FILE__)].each {|f| require f }
+Dir[File.expand_path('support/**/*.rb', __dir__)].each {|f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
